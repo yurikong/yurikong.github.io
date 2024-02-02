58ff71400ce900420cb80f76563605a0e830bf53
Jianqiang Du <jackdu2013@163.com>
2024-02-02 15:40:37 +0800

    feat: added unplugin-icons & remix icon

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'node:path'
+import Icons from 'unplugin-icons/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Icons({
+      scale: 1,
+      compiler: 'jsx',
+      jsx: 'react',
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
