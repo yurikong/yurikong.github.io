f85ffb733de485ba390dc96b129847a91772db9c
Jianqiang Du <jackdu2013@163.com>
2024-02-11 20:48:17 +0800

    feat: toggle AppHeader visibility based on scrolling

--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -1,4 +1,4 @@
-import type { JSX } from 'react'
+import { type JSX, useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import RiMenuLine from '~icons/ri/menu-line'
 import style from './index.module.css'
@@ -7,8 +7,35 @@ import style from './index.module.css'
  * @returns {JSX.Element} the AppHeader component
  */
 export const AppHeader = (): JSX.Element => {
+  const [visible, setVisible] = useState<boolean>(true)
+
+  useEffect(() => {
+    let prevScrollY = 0
+
+    /**
+     * 处理滚动事件
+     */
+    const handleScroll = (): void => {
+      const scrollY = window.scrollY
+
+      // 向上滚动时显示，向下滚动时隐藏
+      setVisible(prevScrollY > scrollY)
+      prevScrollY = scrollY
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  const headerClassName = `${style['header']} ${
+    visible ? style['visible'] : style['hidden']
+  }`.trim()
+
   return (
-    <header className={style['header']}>
+    <header className={headerClassName}>
       {/* Logo */}
       <Link className={style['logo']} to='/'>
         Jack
