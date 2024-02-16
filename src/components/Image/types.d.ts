74b4d697ebde1351d9d90e36080b4da9b1189355
Jianqiang Du <jackdu2013@163.com>
2024-02-16 16:51:40 +0800

    feat: added Image component

---
+++ src/components/Image/types.d.ts
@@ -0,0 +1,7 @@
+export interface ImageProps {
+  name: string
+  alt?: string | undefined
+  watermark?: boolean | undefined
+  watermarkText?: string | undefined
+  className?: string | undefined
+}
