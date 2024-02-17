93156f435df90e47fdab81f351baaa2265f43d2a
Jianqiang Du <jackdu2013@163.com>
2024-02-17 16:38:33 +0800

    refactor: change Project props

--- src/pages/Home/components/Project/index.tsx
+++ src/pages/Home/components/Project/index.tsx
@@ -1,67 +1,64 @@
 import { type JSX, forwardRef, useRef, useImperativeHandle } from 'react'
-import type { ProjectProps, ProjectRef } from './types'
+import type { ProjectRef } from './types'
 import { ProjectItemList, type ProjectItemListRef } from '../ProjectItemList'
-import { StickyHeader, More } from '@/components'
+import { StickyHeader, BackTop } from '@/components'
 import style from './index.module.css'
 
-export type { ProjectProps, ProjectRef }
+export type { ProjectRef }
 
 /**
  * 项目
- * @param {ProjectProps} props 组件入参
- * @param {ProjectProps['onScrollToRef']} props.onScrollToRef 处理 `More` 组件的 `onClick`
  * @returns {JSX.Element} `Project` 组件
  */
-export const Project = forwardRef<ProjectRef, ProjectProps>(function Project(
-  { onScrollToRef },
-  ref
-): JSX.Element {
-  const projectRef = useRef<HTMLElement | null>(null)
-  const projectItemListRef = useRef<ProjectItemListRef | null>(null)
+export const Project = forwardRef<ProjectRef>(
+  function Project(_, ref): JSX.Element {
+    const projectRef = useRef<HTMLElement | null>(null)
+    const projectItemListRef = useRef<ProjectItemListRef | null>(null)
 
-  // 暴露给父组件的方法
-  useImperativeHandle(
-    ref,
-    () => {
-      return {
-        /**
-         * 滚动到该组件
-         * @param {boolean | ScrollIntoViewOptions | undefined} arg 滚动选项
-         */
-        scrollIntoView(
-          arg?: boolean | ScrollIntoViewOptions | undefined
-        ): void {
-          projectRef.current?.scrollIntoView(arg)
-        },
+    // 暴露给父组件的方法
+    useImperativeHandle(
+      ref,
+      () => {
+        return {
+          /**
+           * 滚动到该组件
+           * @param {boolean | ScrollIntoViewOptions | undefined} arg 滚动选项
+           */
+          scrollIntoView(
+            arg?: boolean | ScrollIntoViewOptions | undefined
+          ): void {
+            projectRef.current?.scrollIntoView(arg)
+          },
 
-        /**
-         * 滚动到指定项目
-         * @param {string} projectName 项目名称
-         * @param {boolean | ScrollIntoViewOptions | undefined} arg 滚动选项
-         */
-        scrollToProject(
-          projectName: string,
-          arg?: boolean | ScrollIntoViewOptions | undefined
-        ): void {
-          projectItemListRef.current?.scrollToProjectItem(projectName, arg)
-        },
-      }
-    },
-    []
-  )
+          /**
+           * 滚动到指定项目
+           * @param {string} projectName 项目名称
+           * @param {boolean | ScrollIntoViewOptions | undefined} arg 滚动选项
+           */
+          scrollToProject(
+            projectName: string,
+            arg?: boolean | ScrollIntoViewOptions | undefined
+          ): void {
+            projectItemListRef.current?.scrollToProjectItem(projectName, arg)
+          },
+        }
+      },
+      []
+    )
 
-  return (
-    <section className={style['project']} ref={projectRef}>
-      {/* 标题 */}
-      <StickyHeader title='Project' />
+    return (
+      <section className={style['project']} ref={projectRef}>
+        {/* 标题 */}
+        <StickyHeader title='Project' />
 
-      <div className={style['content']}>
-        {/* 项目列表 */}
-        <ProjectItemList ref={projectItemListRef} />
+        <div className={style['content']}>
+          {/* 项目列表 */}
+          <ProjectItemList ref={projectItemListRef} />
 
-        {/* 滚动到下个部分 */}
-        <More className={style['more']} onClick={onScrollToRef} />
-      </div>
-    </section>
-  )
-})
+          {/* 滚动到最顶部 */}
+          <BackTop className={style['back-top']} />
+        </div>
+      </section>
+    )
+  }
+)
--- src/pages/Home/components/Project/types.d.ts
+++ src/pages/Home/components/Project/types.d.ts
@@ -1,9 +1,3 @@
-import type { MouseEventHandler } from 'react'
-
-export interface ProjectProps {
-  onScrollToRef: MouseEventHandler<HTMLElement>
-}
-
 export interface ProjectRef {
   scrollIntoView: (arg?: boolean | ScrollIntoViewOptions | undefined) => void
   scrollToProject: (
