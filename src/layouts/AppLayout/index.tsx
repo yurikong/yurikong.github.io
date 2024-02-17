72ef3f8ebf536a8c34d06082be84d20fb7c5c9b5
Jianqiang Du <jackdu2013@163.com>
2024-02-17 21:55:41 +0800

    fix: added bottom banner to notify work in progress

--- src/layouts/AppLayout/index.tsx
+++ src/layouts/AppLayout/index.tsx
@@ -1,4 +1,4 @@
-import type { JSX } from 'react'
+import { type JSX, useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import { AppFooter } from '@/components'
 
@@ -7,6 +7,15 @@ import { AppFooter } from '@/components'
  * @returns {JSX.Element} `AppLayout` 组件
  */
 export const AppLayout = (): JSX.Element => {
+  const [bottomBannerVisible, setBottomBannerVisible] = useState(true)
+
+  /**
+   * 处理底栏 onClick 事件
+   */
+  const handleBottomBannerClick = (): void => {
+    setBottomBannerVisible(false)
+  }
+
   return (
     <>
       <main>
@@ -14,6 +23,17 @@ export const AppLayout = (): JSX.Element => {
       </main>
 
       <AppFooter />
+
+      {/* TODO: 完成屏幕适配时删除 */}
+      {bottomBannerVisible && (
+        <div id='bottom-banner'>
+          <div>
+            Currently targeting phone browsers. For tablet and pc users, please
+            view on phone for better experiences.
+          </div>
+          <button onClick={handleBottomBannerClick}>X</button>
+        </div>
+      )}
     </>
   )
 }
