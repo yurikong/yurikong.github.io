f5a74efacd8448dfd29a15520ce5b6ec05771d6e
Jianqiang Du <jackdu2013@163.com>
2024-02-18 11:54:38 +0800

    refactor: refactor BottomBanner

---
+++ src/components/BottomBanner/types.d.ts
@@ -0,0 +1,3 @@
+export interface BottomBannerProps {
+  className?: string | undefined
+}
--- src/layouts/AppLayout/index.tsx
+++ src/layouts/AppLayout/index.tsx
@@ -1,21 +1,12 @@
-import { type JSX, useState } from 'react'
+import type { JSX } from 'react'
 import { Outlet } from 'react-router-dom'
-import { AppFooter } from '@/components'
+import { AppFooter, BottomBanner, AppMenuOverlay } from '@/components'
 
 /**
  * 布局
  * @returns {JSX.Element} `AppLayout` 组件
  */
 export const AppLayout = (): JSX.Element => {
-  const [bottomBannerVisible, setBottomBannerVisible] = useState(true)
-
-  /**
-   * 处理底栏 onClick 事件
-   */
-  const handleBottomBannerClick = (): void => {
-    setBottomBannerVisible(false)
-  }
-
   return (
     <>
       <main>
@@ -24,16 +15,9 @@ export const AppLayout = (): JSX.Element => {
 
       <AppFooter />
 
-      {/* TODO: 完成屏幕适配时删除 */}
-      {bottomBannerVisible && (
-        <div id='bottom-banner'>
-          <div>
-            Currently targeting phone browsers. For tablet and pc users, please
-            view on phone for better experiences.
-          </div>
-          <button onClick={handleBottomBannerClick}>X</button>
-        </div>
-      )}
+      <BottomBanner />
+
+      <AppMenuOverlay />
     </>
   )
 }
