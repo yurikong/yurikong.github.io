b1cf9afa55b60105ee7744d1ccfab364bf791134
Jianqiang Du <jackdu2013@163.com>
2024-03-03 20:58:13 +0800

    feat: intro

--- src/pages/Home/Intro/intro.tsx
+++ src/pages/Home/Intro/intro.tsx
@@ -1,5 +1,14 @@
 import type { JSX } from 'react'
+import style from './intro.module.css'
 
 export const Intro = (): JSX.Element => {
-  return <section>Intro</section>
+  return (
+    <section className={style['intro']}>
+      <h1>Hi, I&apos; Jack.</h1>
+      <p>
+        I am a front-end developer. I make software that looks good and is well built under the hood. I will become a
+        digital nomad in the future.
+      </p>
+    </section>
+  )
 }
